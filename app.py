fe05e6e2f5847039b497f2acbbd094ccfd12928a
kirti101 <85452725+kirti101@users.noreply.github.com>
2022-09-07 17:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,21 +40,6 @@ def predict_Stroke(data:Stroke):
     test_dataset=preprocess_data_test(df)
     test_dataset=test_dataset.to_numpy()
     print(test_dataset)
-    # print(data)
-    # gender=data['gender']
-    # age=data['age']
-    # hypertension=data['hypertension']
-    # heart_disease=data['heart_disease']
-    # ever_married=data['ever_married']
-    # work_type =data['work_type']
-    # Residence_type =data['Residence_type']
-    # avg_glucose_level=data['avg_glucose_level']
-    # bmi=data['bmi']
-    # smoking_status=data['smoking_status']
-    # df=np.array([gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, avg_glucose_level, bmi, smoking_status]).reshape(1, -1)
-    # df=pd.DataFrame(df,columns=['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status'])
-    # test_dataset=preprocess_data(df)
-    # test_dataset=test_dataset.to_numpy()
     prediction = classifier.predict(test_dataset)
     # if(prediction[0]==0):
     if(prediction[0]==0):
